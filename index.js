5ca233d7d7f7d10c7ad0a0ff3e267b1fc7222bbd
Halyna-B <galya.berdik@gmail.com>
2021-05-14 16:43:26 +0300

    added converted values

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// Сonverted to value(m, cm, in, ft) //
+function conTo(){
+const convertTo = document.querySelector('.options-to');
+return convertTo.value;
+}
+//
+
+//Converted from value(m, cm, in, ft) //
+function conFrom(){
+    const convertFrom = document.querySelector('.options-from');
+    return convertFrom.value
+}
+//
 
 
 function lengthConverterToMeter(value){
@@ -7,6 +20,9 @@ function lengthConverterToMeter(value){
 
 const convertBtn = document.querySelector('.convert-btn');
 convertBtn.addEventListener('click', lengthConverterToMeter);
+convertBtn.addEventListener('click', conTo);
+
+
 
 // Объект в JSON формате, содержащий расстояние заданное для конвертации (distance) со
 // значением (value) и шкалой (unit), a также обозначение единицы для шкалы, в которую
